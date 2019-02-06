ada0e11ef210ad08e2ad561826f72e92cef4e654
kevinhaofree <kevinhaofree@users.noreply.github.com>
2019-02-06 10:16:08 -0800

    Delete simpleTry.R

--- simpleTry.R
+++
@@ -1,9 +0,0 @@
-#### we use a simple code for test
-### find integration of x^2 on integral (0,1)
-x = seq(0,1,by = 0.001)
-res = lapply(x,function(x){x^2})
-
-
-message("save for integration")
-## save the result(importantssh)
-save(res,file = "resulthere.RData")

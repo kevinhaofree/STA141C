72a738ed24dba94f57115651abfb25567a033466
haost <ssthao@ucdavis.edu>
2019-02-06 09:20:56 -0800

    submit a version1

--- simpleTry.R
+++ simpleTry.R
@@ -2,5 +2,8 @@
 ### find integration of x^2 on integral (0,1)
 x = seq(0,1,by = 0.001)
 res = lapply(x,function(x){x^2})
+
+
 message("save for integration")
+## save the result(importantssh)
 save(res,file = "resulthere.RData")
